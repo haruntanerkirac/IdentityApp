e87015e903bb754b3ae5c75edbfc3f9f47bee9fe
haruntanerkirac <haruntanerkirac@gmail.com>
2025-03-20 21:47:28 +0300

    kullanıcı oluşturma işlemi

---
+++ IdentityApp.Web/ViewModels/CreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Web.ViewModels
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Girdiğiniz parolalar eşleşmiyor !")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
