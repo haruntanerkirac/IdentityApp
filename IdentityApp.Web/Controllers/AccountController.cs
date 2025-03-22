869b84ee4150f26336c6772992d75a0633f55717
haruntanerkirac <haruntanerkirac@gmail.com>
2025-03-22 03:25:34 +0300

    login işlemi

--- IdentityApp.Web/Controllers/AccountController.cs
+++ IdentityApp.Web/Controllers/AccountController.cs
@@ -1,13 +1,65 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityApp.Web.Models;
+using IdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    var result = await _signInManager.PasswordSignInAsync(user,model.Password, model.RememberMe,true);
+                    if (result.Succeeded)
+                    {
+                        await _userManager.ResetAccessFailedCountAsync(user);
+                        await _userManager.SetLockoutEndDateAsync(user,null);
+
+                        return RedirectToAction("Index","Home");
+                    }
+                    else if (result.IsLockedOut)
+                    {
+                        var lockoutDate = await _userManager.GetLockoutEndDateAsync(user);
+                        var timeLeft = lockoutDate.Value - DateTime.UtcNow;
+                        ModelState.AddModelError("",$"Hesabınız kitlendi ! Lütfen {timeLeft.Minutes + 1} dakika sonra tekrar deneyiniz.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Hatalı email veya parola !");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı email veya parola !");
+                }
+            }
+
+            return View(model);
         }
     }
 }
